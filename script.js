e5de9557b9146903d9a14bca7d52fa37ad1e4817
Kim <kimleawel@gmail.com>
2023-06-12 22:02:18 +0200

    initialising the folder

---
+++ script.js
@@ -0,0 +1,42 @@
+// BUTTONS AND TEXT DIVS
+    // BUTTON 1
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('button_1');
+    const div = document.getElementById('text_1');
+  
+    button.addEventListener('click', function() {
+      if (div.style.display === 'none') {
+        div.style.display = 'block';
+      } else {
+        div.style.display = 'none';
+      }
+    });
+  });
+
+  // BUTTON 2
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('button_2');
+    const div = document.getElementById('text_2');
+  
+    button.addEventListener('click', function() {
+      if (div.style.display === 'none') {
+        div.style.display = 'block';
+      } else {
+        div.style.display = 'none';
+      }
+    });
+  });
+
+  // BUTTON 3
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('button_3');
+    const div = document.getElementById('text_3');
+  
+    button.addEventListener('click', function() {
+      if (div.style.display === 'none') {
+        div.style.display = 'block';
+      } else {
+        div.style.display = 'none';
+      }
+    });
+  });
